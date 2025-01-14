90bdf5c913356952b941d664588b9cf04df8c8fd
emeraldhycient <igwezehycient86@gmail.com>
2025-01-14 20:04:01 +0200

    fix(meet-requirement): cleaning up to meet the task guidline

--- components/cookies/components/banner-actions.tsx
+++ components/cookies/components/banner-actions.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/react";
 import React from "react";
 
 type BannerActionsProps = {
@@ -8,26 +9,27 @@ type BannerActionsProps = {
 export function BannerActions({ onAccept, onDecline }: BannerActionsProps) {
   return (
     <div className="flex flex-row justify-end gap-2">
-      <button
+      <Button
         aria-label="Accept cookies"
         className="
           rounded bg-blue-600 px-4 py-2 text-white
           hover:bg-blue-700 focus:outline-none focus:ring
         "
-        onClick={onAccept}
+        onPress={onAccept}
       >
         Accept
-      </button>
-      <button
+      </Button>
+      <Button
         aria-label="Decline cookies"
         className="
           rounded border border-gray-500 px-4 py-2 text-gray-200
           hover:bg-gray-700 focus:outline-none focus:ring
         "
-        onClick={onDecline}
+        variant="bordered"
+        onPress={onDecline}
       >
         Decline
-      </button>
+      </Button>
     </div>
   );
 }
